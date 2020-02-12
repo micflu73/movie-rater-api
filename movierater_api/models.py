124d33595e37c9803509b9f565c5acf84491dda1
Michael Fluch <michael.fluch@mail.com>
2020-02-12 01:25:57 +0100

    Back-end API fully implemented

--- movierater_api/models.py
+++ movierater_api/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+class Movie(models.Model):
+    """ Movie to be ratetd """
+    title = models.CharField(max_length=32)
+    description = models.TextField(max_length=360)
+
+    def no_of_ratings(self):
+        """get number of ratings for the movie"""
+        ratings = Rating.objects.filter(movie=self)
+        return len(ratings)
+
+    def avg_rating(self):
+        """get average rating for the movie"""
+        sum = 0
+        ratings = Rating.objects.filter(movie=self)
+        for rating in ratings:
+            sum += rating.stars
+        if len(ratings) > 0:
+            return sum / len(ratings)
+        else:
+            return 0
+
+    def __str__(self):
+        """return string represention"""
+        return self.title
+
+class Rating(models.Model):
+    """ Rating of a movie """
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    stars = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
+
+    class Meta:
+        # anly one rating per user and movie
+        unique_together = (('user', 'movie'),)
+        index_together = (('user', 'movie'),)
+
+    def __str__(self):
+        """return string represention"""
+        return str(self.id)
